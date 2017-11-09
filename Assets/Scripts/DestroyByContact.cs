9a6291779276e6d869ec5394a06d234b86f0d05c
Anthony Lopez <smashmaster87@hotmail.com>
2017-11-09 18:53:11 -0500

    Initial commit

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour 
+{
+	public GameObject explosion;
+	public GameObject playerExplosion;
+	
+	void OnTriggerEnter(Collider other) 
+    {
+        if (other.tag == "Boundary")
+        {
+            return;
+        }
+
+		Instantiate(explosion, transform.position, transform.rotation);
+
+		GetComponent<AudioSource> ().Play ();
+		
+		if (other.tag == "Player")
+		{
+			Debug.Log("ITS A PLAYA");
+			Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+		}
+        
+		Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
+
