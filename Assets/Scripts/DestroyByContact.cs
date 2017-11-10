65dc18206035c1759a33ef6764c4ffda9da8c4e7
Anthony Lopez <smashmaster87@hotmail.com>
2017-11-09 19:55:44 -0500

    Working on Different GUITexts

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -7,6 +7,21 @@ public class DestroyByContact : MonoBehaviour
 { 
 	public GameObject explosion;
 	public GameObject playerExplosion;
+	public int scoreValue;
+	private GameController gameController;
+
+	void Start ()
+	{
+		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+		if (gameControllerObject != null)
+		{
+			gameController = gameControllerObject.GetComponent <GameController>();
+		}
+		if (gameController == null)
+		{
+			Debug.Log ("Cant find 'GameController' script");
+		}
+	}
 	
 	void OnTriggerEnter(Collider other) 
     {
@@ -21,10 +36,9 @@ public class DestroyByContact : MonoBehaviour
 		
 		if (other.tag == "Player")
 		{
-			Debug.Log("ITS A PLAYA");
 			Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
 		}
-        
+        gameController.AddScore (scoreValue);
 		Destroy(other.gameObject);
         Destroy(gameObject);
     }
